4ef3a471efa888c1f726e6fd2e9da24d5de5da45
jaimin <jaiminbhatt.mscit19@vnsgu.ac.in>
2022-10-21 15:06:58 +0530

    dashboard master 1.0

---
+++ src/app/dashboard-master/dashboard-master.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DashboardMasterComponent } from './dashboard-master.component';
+
+describe('DashboardMasterComponent', () => {
+  let component: DashboardMasterComponent;
+  let fixture: ComponentFixture<DashboardMasterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DashboardMasterComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DashboardMasterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
